b999e89e03d96bd5de87d990e7e35b6b242b3bb0
Filipe Soares <filipeseventeen1@gmail.com>
2021-08-09 00:45:01 -0300

    style: responsive done in defended teams

--- src/components/DefendedTeams/style.ts
+++ src/components/DefendedTeams/style.ts
@@ -5,10 +5,15 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
 
-  margin-top: -50px;
-
   h1 {
     color: #a0a0a0;
+    font-size: 32px;
+  }
+
+  @media (max-width: 720px) {
+    h1 {
+      font-size: 22px;
+    }
   }
 `
 
@@ -16,6 +21,11 @@ export const Wrapper = styled.div`
   display: flex;
   gap: 28px;
   padding: 32px;
+
+  @media (max-width: 720px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+  }
 `
 
 export const Team = styled.div`
@@ -41,5 +51,16 @@ export const Team = styled.div`
     text-align: center;
     padding: 12px;
     font-size: 18px;
+  }
+
+  @media (max-width: 720px) {
+    div {
+      width: 117px;
+      height: 122px;
+
+      img {
+        height: 80px;
+      }
+    }
   }
 `
