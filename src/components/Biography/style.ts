175ccb57acededa7a7d3b3073e071f633205d157
Filipe Soares <filipeseventeen1@gmail.com>
2021-08-10 19:46:59 -0300

    style: added animation to know more button

--- src/components/Biography/style.ts
+++ src/components/Biography/style.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
 export const Container = styled.div`
   position: relative;
@@ -51,6 +51,16 @@ export const Wrapper = styled.div`
   }
 `
 
+const bouncing = keyframes`
+  from {
+    transform: translateY(-2px);
+  }
+ 
+  to {
+    transform: translateY(2px);
+  }
+`
+
 export const KnowMoreButton = styled.button`
   margin: auto;
 
@@ -60,10 +70,10 @@ export const KnowMoreButton = styled.button`
   font-weight: 500;
   font-size: 14px;
 
-  position: absolute;
-  top: 12px;
-  left: 50%;
-  transform: translateX(-50%) translateY(-50%);
+  transition: transform 0.1s;
+  animation: ${bouncing} 1s;
+
+  animation-iteration-count: infinite;
 
   .icon {
     fill: #a0a0a0;
