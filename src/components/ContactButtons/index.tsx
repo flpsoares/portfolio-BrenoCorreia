eb1b0ddb189b683f6b6eb8b5e1df7ae55cf8ccec
Filipe Soares <filipeseventeen1@gmail.com>
2021-08-12 15:09:06 -0300

    feat: added links to contact buttons

--- src/components/ContactButtons/index.tsx
+++ src/components/ContactButtons/index.tsx
@@ -3,10 +3,13 @@ import { Container, Email, Whatsapp } from './style'
 export const ContactButtons: React.FC = () => {
   return (
     <Container>
-      <Email href="#">
+      <Email target="_blank" href="mailto:po.fa19701104@gmail.com">
         <img src="/assets/contact/email.svg" alt="email" />
       </Email>
-      <Whatsapp href="#">
+      <Whatsapp
+        target="_blank"
+        href="https://api.whatsapp.com/send?phone=5511981432729"
+      >
         <img src="/assets/contact/whatsapp.svg" alt="whatsapp" />
       </Whatsapp>
     </Container>
