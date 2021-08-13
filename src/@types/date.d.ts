bc5120c9b13fba23ac7c0dc40c38a596b68f7498
Filipe Soares <filipeseventeen1@gmail.com>
2021-08-12 23:06:42 -0300

    feat: added more types to date

--- src/@types/date.d.ts
+++ src/@types/date.d.ts
@@ -4,5 +4,18 @@ declare namespace App {
     date?: number
     team?: string
     isActive?: boolean
+    title?: string
+    title1?: string
+    title2?: string
+    subTitle1?: string
+    subTitle2?: string
+    award1?: string
+    award2?: string
+    subAward1?: string
+    subAward2?: string
+    description?: string
+    imagem1?: string
+    imagem2?: string
+    imagem3?: string
   }
 }
